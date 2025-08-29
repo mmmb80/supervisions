b7a21e0176cf20fa25faa35f87ce7d18d5c54fa8
Balint Molnar <molnar.balint23@gmail.com>
2025-08-29 23:38:01 +0100

    Add links

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -2,15 +2,18 @@ import type { Config } from '@docusaurus/types';
 
 const config: Config = {
   title: 'My Website',
-  url: 'https://mmmb80.github.io', // your GitHub username
-  baseUrl: '/supervisions/',                  // your repo name; use '/' if repo is <username>.github.io
+  url: 'https://mmmb80.github.io', 
+  baseUrl: '/supervisions/',                 
   projectName: 'supervisions',
   organizationName: 'mmmb80',
   presets: [
     [
       'classic',
       {
-        docs: false,
+        docs: {
+          sidebarPath: require.resolve('./sidebars.ts'),
+          editUrl: 'https://github.com/mmmb80/supervisions/tree/main/',
+        },
         blog: false,
       },
     ],
---
+++ src/pages/index.md
@@ -0,0 +1,9 @@
+# My Documents
+## PDFs
+* [complexity1.pdf](/pdfs/complexity1.pdf)
+* [main.pdf](/pdfs/main.pdf)
+* [sv2.pdf](/pdfs/sv2.pdf)
+* [sv3.pdf](/pdfs/sv3.pdf)
+* [sv4.pdf](/pdfs/sv4.pdf)
+* [sv5.pdf](/pdfs/sv5.pdf)
+* [sv6.pdf](/pdfs/sv6.pdf)
